c2f4770cd53264438df87923f5f720a4c79ef4dd
Andrew Meier <ameier38@gmail.com>
2017-02-18 17:12:33 -0500

    cleanup

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,4 +2,5 @@ import os
 
 # Specify to auto load the 'tasks.py' module.
 imports = ("tasks",)
+broker_url = os.getenv('CELERY_BROKER_URL', 'amqp://')
 result_backend = os.getenv('CELERY_RESULT_BACKEND_URL', 'redis://')
--- tasks.py
+++ tasks.py
@@ -2,11 +2,10 @@ import os
 import time
 from celery import Celery
 
-broker = os.getenv('CELERY_BROKER_URL', 'amqp://')
-result_backend = os.getenv('CELERY_RESULT_BACKEND_URL', 'redis://')
+app = Celery('tasks')
 
-app = Celery('tasks', broker=broker)
-# set this separately because of issue with Windows
+# set backend again separately because of issue with Windows
+result_backend = os.getenv('CELERY_RESULT_BACKEND_URL', 'redis://')
 app.conf.update(result_backend=result_backend)
 
 @app.task 
